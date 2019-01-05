60c60f029cad8e5519280c2ba32109b2f418d50a
j0811s <parkmill0811@gmail.com>
2019-01-06 01:48:43 +0900

    gulp add

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@
 const gulp = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
  
 // style.scssの監視タスクを作成する
 gulp.task('default', function () {
   // ★ style.scssファイルを監視
-  return gulp.watch('css/style.scss', function () {
+  return gulp.watch('css/scss/style.scss', function () {
     // style.scssの更新があった場合の処理
- 
     // style.scssファイルを取得
-    return gulp.src('css/style.scss')
+    return gulp.src('css/scss/style.scss')
+      //autoprefixerの実行
+      .pipe(autoprefixer({
+        browsers: ['last 2 Chrome versions'],
+        cascade: false
+      }))
       // Sassのコンパイルを実行
       .pipe(sass({
         outputStyle: 'expanded'
